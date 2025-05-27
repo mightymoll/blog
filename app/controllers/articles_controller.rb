39ae6c694e4aa606439b98370bdc3e94aa99eb0d
molly <mnfelts@gmail.com>
2025-05-27 10:04:31 +0200

    display list of articles
    - controller : fetch all articles action
    - view : display all article titles in list format

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,7 @@ class ArticlesController < ApplicationController
   # Rails will automatically render a view that matches the name of the controller and action
   # # ie app/views/articles/index.html.erb
   def index
+    # fetch all articles from the database
+    @articles = Article.all
   end
 end
