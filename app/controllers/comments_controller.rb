98ebe3a669a439b7e2fd15d0f08afe27be3c1129
molly <mnfelts@gmail.com>
2025-05-27 19:33:57 +0200

    Comments Controller for articles
    - create a comment, keeping track of which article it's related to
    
    - show view display commenter name and comments as well as a form to add a comment on article's page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  # create a comment, keeping track of which article it's related to
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
