638119640475a7d421a7d637208b4b5edf5f6710
molly <mnfelts@gmail.com>
2025-05-27 10:37:23 +0200

    resourceful routing
    - use resources @ routes to generate basic CRUD functions for articles (simplifies/automates routing)
    - use helpers @ articles view to display title links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # set articles index as root (main) page
   root "articles#index"
-  # GET /articles requests are mapped to the index action of ArticlesController
-  get "/articles", to: "articles#index"
-  # GET /article requests are mapped to the SHOW action of ArticlesController based on id parameter
-  get "/articles/:id", to: "articles#show"
+
+  # 'resources' maps all of the conventional routes for CRUD
+  resources :articles
 end
