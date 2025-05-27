4f42a8be327e9390c9b03ab1fefc48487002d2af
molly <mnfelts@gmail.com>
2025-05-27 09:52:57 +0200

    - created ArticlesController and its index action using the controller generator (with --skip-routes option because the route already exists): $ bin/rails generate controller Articles index --skip-routes
    - created an Articles model using the cl & migrated to the db
    
    - created a new Article object using the cl

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,6 @@
+class ArticlesController < ApplicationController
+  # Rails will automatically render a view that matches the name of the controller and action
+  # # ie app/views/articles/index.html.erb
+  def index
+  end
+end
